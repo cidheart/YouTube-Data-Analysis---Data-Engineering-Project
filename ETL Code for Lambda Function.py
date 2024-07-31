7e47f03635a041461969a8f1008455992b2d6836
cidheart <140436992+cidheart@users.noreply.github.com>
2024-08-01 00:17:05 +0530

    Add files via upload

---
+++ ETL Code for Lambda Function.py
@@ -0,0 +1,43 @@
+import awswrangler as wr #for accessing files in s3
+import pandas as pd
+import urllib.parse #helps manipulating with URLs and their inner components like http,path etc
+import os #helps creating env variables
+
+os_input_s3_cleansed_layer = os.environ['s3_cleansed_layer'] #output path
+os_input_glue_catalog_db_name = os.environ['glue_catalog_db_name'] #db name
+os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name'] #table name
+os_input_write_data_operation = os.environ['write_data_operation'] #append in this case
+#Decouple Configuration from Code: Keep configuration data separate from your code, making your function more flexible
+#and easier to manage.
+#Securely Store Sensitive Information: Environment variables can store sensitive information such as database
+#credentials, API keys, or other secrets without hardcoding them into your source code.
+#Easily Update Configuration: Change the configuration settings without modifying and redeploying your code.
+
+
+def lambda_handler(event, context):
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+
+        # Creating DF from content
+        df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
+
+        # Extract required columns:
+        df_step_1 = pd.json_normalize(df_raw['items'])
+
+        # Write to S3
+        wr_response = wr.s3.to_parquet(
+            df=df_step_1,
+            path=os_input_s3_cleansed_layer,
+            dataset=True,
+            database=os_input_glue_catalog_db_name,
+            table=os_input_glue_catalog_table_name,
+            mode=os_input_write_data_operation
+        )
+
+        return wr_response
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
